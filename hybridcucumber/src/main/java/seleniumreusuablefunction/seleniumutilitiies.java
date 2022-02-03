4f61835eef2d95750e384f6cad6c6133edfcdce0
rethisha <rethisha@NICDM203.nousinfo.com>
2022-02-03 09:50:02 +0530

    hybrid

--- hybridcucumber/src/main/java/seleniumreusuablefunction/seleniumutilitiies.java
+++
@@ -1,5 +0,0 @@
-package seleniumreusuablefunction;
-
-public class seleniumutilitiies {
-
-}
--- hybridcucumber/src/test/java/testcases/logintestcase.java
+++ hybridcucumber/src/test/java/testcases/logintestcase.java
@@ -1,5 +1,34 @@
 package testcases;
 
-public class logintestcase {
+import java.io.IOException;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import baseclass.libraryclass;
+import pages.loginpage;
+import seleniumreusuablefunction.seleniumutilities;
+
+public class logintestcase extends libraryclass {
+	seleniumutilities selenium;
+	loginpage lp;
+	@BeforeMethod
+	public void openapp()
+	{
+		launchapp("chrome","https://opensource-demo.orangehrmlive.com/");
+	}
+	@Test
+	public void login()
+	{
+		 lp=new loginpage(driver);
+		lp.login("Admin","admin123");
+	}
+	
+	@AfterMethod
+	public void teardown() throws IOException
+	{
+	selenium=new seleniumutilities(driver);
+	 selenium.screenshot("C:\\Users\\rethisha\\git\\hybridcucumber\\hybridcucumber\\src\\test\\resources\\screenshots\\Login.png");
+	}
 }
